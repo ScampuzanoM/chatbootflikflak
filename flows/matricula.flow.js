8666069976aae6eea135a10de80dad5b55ff7ba9
ScampuzanoM <sebascm3450@gmail.com>
2024-02-26 18:22:59 -0500

    fix: :zap: ajuste mensaje

--- flows/matricula.flow.js
+++ flows/matricula.flow.js
@@ -5,17 +5,17 @@ const welcomeFlow = require("./welcome.flow");
  * Flujo de bienvenida
  */
 
-const TEL = '3043760881'
+const TEL = '+573043760881'
 
 module.exports = addKeyword(['4']).addAnswer(
     [
         '¡Seras dirigido con un asesor persolanizado que te ayudara con tu proceso de matricula.! 🌟',
-        
+
     ], null, async(ctx,{flowDynamic, state}) => {
         const myState = state.getMyState();
         const msn = `Hola estoy ${myState.nombre} interesado en el proceso de matricula mi edad es ${myState.edad}`
         const link = encodeURI(`https://wa.me/${TEL}?text=${msn}`);
-        await flowDynamic(`Edad! ${link}`)
+        await flowDynamic(`**Dar click en el siguiente link** ${link}`)
 
     }
     )
