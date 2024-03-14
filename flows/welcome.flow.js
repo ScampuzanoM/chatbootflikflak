552d35f0b4d20a6721049b4f166f16ed82be9e1e
Santihm145 <80368083+Santihm145@users.noreply.github.com>
2024-03-14 15:50:14 -0500

    feat:

--- flows/welcome.flow.js
+++ flows/welcome.flow.js
@@ -16,14 +16,10 @@ module.exports =  addKeyword(['hola', 'ole', 'alo','buenas','menu'])
 .addAnswer('🙌 ¡Hola FlikFlaker! Bienvenido/a a un mundo lleno de piruetas con *Flik-Flak*. Soy tu asistente virtual, *FlikFlakBot*.')
 .addAnswer(
     [
-        'Explora las emocionantes opciones que tenemos para ti. ¿Listo/a para sumergirte en el fascinante universo de la gimnasia artística? 🌍',
-        '',
-        '**Menú:**',
-        '1. 🌐 Identificar si ya eres cliente Accede a tu cuenta y descubre promociones exclusivas.',
-        '   - Realiza trámites de pagos de mensualidades de manera rápida y sencilla.',
-        '2. 👋 Nuevo FlikFlaker: ¡Bienvenido/a al equipo! Explora las opciones para empezar tu viaje con *Flik-Flak*.',
-        '3. 📋 Pólizas: Conoce nuestras políticas y términos.',
-        '0. ☎️ Contactar un asesor: Deja tu número y uno de nuestros asesores se comunicará contigo para ofrecerte la mejor asesoría de una manera ¡divertida y personalizada!',
+        '*Menú:*',
+        '1. 🌐 Ya eres cliente.',
+        '2. 👋 Nuevo FlikFlaker.',
+        '3. 📋 Pólizas.',
         '',
         '¡Comencemos tu viaje de *Flik-Flak* juntos! ¿En qué puedo ayudarte hoy? 😊✨',
     ],
@@ -43,10 +39,10 @@ module.exports =  addKeyword(['hola', 'ole', 'alo','buenas','menu'])
                 gotoFlow(polizasFlow)
                 break; 
             } 
-            case '0': { 
-                gotoFlow(asesorFlow)
-                break; 
-            } 
+            // case '0': { 
+            //     gotoFlow(asesorFlow)
+            //     break; 
+            // } 
             default: { 
                 gotoFlow(defaultFlow)
                 return fallBack()
